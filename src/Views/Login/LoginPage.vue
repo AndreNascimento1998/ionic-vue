d6dabc411b95e782675d9a2540de93511d1cfc65
André Linux <andre.ncardoso@hotmail.com>
2023-03-29 13:01:05 -0300

    feat: Página login com validação criada

--- src/Views/Login/LoginPage.vue
+++ src/Views/Login/LoginPage.vue
@@ -1,8 +1,74 @@
 <template>
-    <h1>Login Page</h1>
-    <router-link to="/lista">
-        <button>Tela de Lista</button>
-    </router-link>
-</template>
+    <ion-page>
+        <ion-header>
+            <ion-toolbar color="primary">
+                <ion-title>Login</ion-title>
+            </ion-toolbar>
+        </ion-header>
 
-<script setup></script>
+        <ion-content>
+            
+                <ion-row class="margin-top">
+                    <ion-col size-md="3" offset-md="4" size="8" offset="2">
+                        <ion-item >
+                            <ion-label position="floating">Email</ion-label>
+                            <ion-input v-model="email" class="ion-input-outlined " type="email"></ion-input>
+                        </ion-item>
+                    </ion-col>
+                </ion-row>
+
+                <ion-row>
+                    <ion-col size-md="3" offset-md="4" size="8" offset="2">
+                        <ion-item >
+                            <ion-label position="floating">Senha</ion-label>
+                            <ion-input v-model="senha" type="password"></ion-input>
+                        </ion-item>
+                    </ion-col>
+                </ion-row>
+
+                <ion-row>
+                    <ion-col size-md="3" offset-md="4" size="8" offset="2">
+                        <ion-button @click.prevent="valida" expand="block" color="primary">Login</ion-button>
+                    </ion-col>
+                </ion-row>
+            
+        </ion-content>
+    </ion-page>
+</template>
+  
+<script setup>
+import router from '@/router';
+import { useGlobal } from '@/stores';
+import '@ionic/core/css/ionic.bundle.css';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonRow, IonCol, IonItem, IonLabel, IonInput, IonButton } from '@ionic/vue';
+import { computed, ref } from 'vue';
+
+
+const store = useGlobal()
+const email = ref('')
+const senha = ref('')
+
+const users = computed(() => store.users)
+
+function valida(){
+    debugger
+    const temp = users.value
+    const filtraId = temp.filter( (user) => email.value == user.email)
+    if(filtraId[0]?.senha == senha.value){
+        router.push('/lista')
+    }else {
+        alert('Senha ou email inválidos')
+    }
+}
+
+</script>
+
+<style scoped>
+.margin-top{
+    margin-top: 30px;
+}
+
+.altura{
+    height: 50%;
+}
+</style>
