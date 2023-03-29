55dc6ebb8014898a5490b03ee72d4f2742ceaff0
André Linux <andre.ncardoso@hotmail.com>
2023-03-29 18:17:59 -0300

    nova página

---
+++ src/Views/Descricao/DescricaoPage.vue
@@ -0,0 +1,48 @@
+
+<template>
+    <ion-card class="alinhament-centro">
+        <ion-card-header>
+            <ion-card-title>{{ usuario.nome }}</ion-card-title>
+            <ion-card-subtitle>{{ usuario.email }}</ion-card-subtitle>
+        </ion-card-header>
+        <ion-row>
+            <ion-col size="2" offset="3">
+                <ion-card-content>
+                    mora no estado {{ usuario.estado }}
+                </ion-card-content>
+            </ion-col>
+            <ion-col size="2">
+                <ion-card-content>
+                    telefone: {{ usuario.telefone }}
+                </ion-card-content>
+            </ion-col>
+            <ion-col size="2">
+                <ion-card-content>
+                    na rua {{ usuario.rua }}
+                </ion-card-content>
+            </ion-col>
+        </ion-row>
+    </ion-card>
+</template>
+
+
+<script setup>
+import { IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent, IonRow, IonCol } from '@ionic/vue';
+import { useGlobal } from '@/stores'
+import { useRoute } from 'vue-router'
+
+const store = useGlobal()
+
+const route = useRoute()
+const userId = route.params.id
+
+const usuario = store.users.find(user => user.id == userId)
+</script>
+
+<style scoped>
+.alinhament-centro{
+    text-align: center;
+    justify-content: center;
+    align-items: center;
+}
+</style>
