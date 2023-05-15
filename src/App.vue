162949271ea926525fdeb03cda6dd3037c9dc103
André Linux <andre.ncardoso@hotmail.com>
2023-05-15 16:03:26 -0300

    fix: dependencias

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,4 @@
 
 <script setup></script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-}
-</style>
+<style scoped></style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
 import { IonicVue } from '@ionic/vue';
+import 'ionicons';
 
 const app = createApp(App)
 app.use(router) 
